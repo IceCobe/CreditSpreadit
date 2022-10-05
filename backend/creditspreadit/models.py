0a1f2ebca6775bcbb52cd66ed595b33202564300
IceCobe <jaalaba@gmail.com>
2022-10-05 17:47:31 -0400

    added date ranges to models for credit card bonus quarters

--- backend/creditspreadit/models.py
+++ backend/creditspreadit/models.py
@@ -4,6 +4,18 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+# Run this to nuke everything
+# python manage.py migrate --run-syncdb
+
+class DateRanges(models.Model):
+    name = models.CharField(max_length=30, verbose_name="Quarter")
+
+    active_start = models.DateField(verbose_name="Activate Date Start")
+    active_end = models.DateField(verbose_name="Activate Date End")
+
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
     category = models.CharField(max_length=30, verbose_name="Category")
 
@@ -12,10 +24,11 @@ class Category(models.Model):
 
 class Bonus(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Bonus Category")
+    dates = models.ForeignKey(DateRanges, on_delete=models.CASCADE, verbose_name="Active Date Range")
     cashback = models.IntegerField(verbose_name="Cashback Percentage")
 
     def __str__(self):
-        return "{}% cashback on {}".format(self.cashback, self.category)
+        return "{}% cashback on {} during {}".format(self.cashback, self.category, self.dates)
 
 class CreditCard(models.Model):
     name = models.CharField(max_length=30, verbose_name="Card Name")
