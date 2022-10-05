217f3f170ccbcb77b574dbcebc8e4d8663c46860
IceCobe <jaalaba@gmail.com>
2022-10-04 20:30:23 -0400

    initial models created

--- backend/creditspreadit/models.py
+++ backend/creditspreadit/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class CreditCard(models.Model):
+    name = models.CharField(max_length=30, verbose_name="Card Name")
+    company = models.CharField(max_length=30, verbose_name="Card Company")
