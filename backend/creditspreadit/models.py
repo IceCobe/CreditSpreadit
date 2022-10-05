d352acacf037be1dc6058a331569cdb5e45fbb0e
IceCobe <jaalaba@gmail.com>
2022-10-04 23:48:02 -0400

    models added for categories and bonuses

--- backend/creditspreadit/models.py
+++ backend/creditspreadit/models.py
@@ -1,6 +1,26 @@
+from tabnanny import verbose
+from unicodedata import category
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=30, verbose_name="Category")
+
+    def __str__(self):
+        return self.category
+
+class Bonus(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Bonus Category")
+    cashback = models.IntegerField(verbose_name="Cashback Percentage")
+
+    def __str__(self):
+        return "{}% cashback on {}".format(self.cashback, self.category)
+
 class CreditCard(models.Model):
     name = models.CharField(max_length=30, verbose_name="Card Name")
     company = models.CharField(max_length=30, verbose_name="Card Company")
+    bonuses = models.ManyToManyField(Bonus, verbose_name="Bonuses")
+
+    def __str__(self):
+        return self.name
